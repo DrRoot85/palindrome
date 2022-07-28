5976112668743606b501135162948acc3d092d69
DrRoot85 <mhbmis@icloud.com>
2022-07-28 13:03:26 +0300

    Add index.js

--- index.js
+++ index.js
@@ -1,54 +1,21 @@
-// Returns true for a palindrome, false otherwise.
-function palindrome(string) {
-  let processedContent = string.toLowerCase();
-  return processedContent === reverse(processedContent);
+module.exports https://github.com/DrRoot85/palindrome
+
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
 }
 
-function emailParts(email) {
-    return email.toLowerCase().split('@');
-  }
-
-String.prototype.blank = function() {
-  return !!(this.blank() = /\s+/g);
-}
-
-// Reverses a string.
-function reverse(string) {
-  return Array.from(string).reverse().join("");
-}
-
+// Defines a Phrase object.
 function Phrase(content) {
   this.content = content;
 
-  this.processor = function(string) {
-    return this.string = string.toLowerCase();
-
-    // Returns true if the phrase is a palindrome, false otherwise.
-  this.palindrome = function palindrome() {
-    return this.processedContent() === this.processedContent().blank();
-  }
-
+  // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.processor(this.content);
+    return this.content.toLowerCase();
   }
 
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
-    return this.processedContent() === reverse(this.processedContent());
+    return this.processedContent() === this.processedContent().reverse();
   }
 }
-
-function TranslatedPhrase(content, translation) {
-  this.content = content;
-  this.translation = translation;
-
-  // Returns translation processed for palindrome testing.
-  this.processedContent = function processedContent() {
-    return this.processor(this.translation);
-  }
-}
-
-
-TranslatedPhrase.prototype = new Phrase();
-
-
